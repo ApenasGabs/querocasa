696f2a3604eb076b64fe594d90dcbebadb36dc80
Gabriel Rodrigues <gabers357@gmail.com>
2024-09-13 19:05:55 -0300

    feat: Adicionar lazy loading para imagens e atualizar ícone da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import lua from "./assets/lua.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="" target="_blank">
+          {count % 2 !== 0 && <h1 className="read-the-docs">Eu também🥹 </h1>}
+          {count % 2 !== 0 && (
+            <img src={lua} className="logo" alt="Vite logo" />
+          )}
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="" target="_blank">
+          <img
+            src={"https://pacaembu.com/svg/ic-mcmv.svg"}
+            className="logo react"
+            alt="imagem de uma casa girando"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount((count) => count + 1)}>Quero 🏡</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
