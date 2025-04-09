1637318a796f856b10e465384a7b804b755badc2
Gabriel Rodrigues <gabers357@gmail.com>
2025-04-08 23:25:57 -0300

    feat: ✨ Adiciona função para obter horário de Brasília e atualiza lógica de processamento de resultados

---
+++ scripts/getBrasiliaTime.js
@@ -0,0 +1,14 @@
+const getBrasiliaTime = () => {
+  const now = new Date();
+  const options = {
+    timeZone: "America/Sao_Paulo",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  return new Intl.DateTimeFormat("pt-BR", options).format(now);
+};
+export default getBrasiliaTime;
